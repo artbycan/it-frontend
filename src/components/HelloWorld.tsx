6c0c2d58856f06e3c173115ace0fb6f288e062df
samart <artbycan.com@gmail.com>
2025-03-16 09:54:02 +0700

    begin

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div className="hello-world">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
